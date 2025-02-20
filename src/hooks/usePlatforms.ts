f99bed45c3b6a6aff00efd275ee4d7e74f61d852
肖帆 <xiaofandev@126.com>
2025-02-20 20:24:26 +0800

    Update platforms fetching url

--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -7,7 +7,7 @@ export interface Platform {
 }
 
 const usePlatforms = () => {
-  return useData<Platform>("/platforms");
+  return useData<Platform>("/platforms/lists/parents");
 };
 
 export default usePlatforms;
