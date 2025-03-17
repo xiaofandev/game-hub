2e2ba0dcb2a56d1d7c9042ff82c9be6a1069408e
肖帆 <xiaofandev@126.com>
2025-03-17 15:15:02 +0800

    Feature: Implement SearchBar

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
-import { Flex } from "@chakra-ui/react";
+import { Flex, Stack } from "@chakra-ui/react";
 import GenreList from "./components/GenreList";
 import Navigation from "./components/Navigation";
 import GameList from "./components/GameList";
+import SearchBar from "./components/SearchBar";
 
 function App() {
   return (
@@ -9,7 +10,13 @@ function App() {
       <Navigation></Navigation>
       <Flex pl={4}>
         <GenreList />
-        <GameList />
+        <Stack>
+          <SearchBar
+            onSelectOrderBy={(orderBy) => console.log(orderBy)}
+            onSelectPlatform={(platform) => console.log(platform)}
+          />
+          <GameList />
+        </Stack>
       </Flex>
     </>
   );
---
+++ src/components/SearchBar.tsx
@@ -0,0 +1,51 @@
+import { HStack, Select, Text } from "@chakra-ui/react";
+import useData from "../hooks/useData";
+
+interface Props {
+  onSelectOrderBy: (orderBy: string) => void;
+  onSelectPlatform: (platform: string) => void;
+}
+
+interface Platform {
+  id: string;
+  name: string;
+}
+
+const SearchBar = ({ onSelectOrderBy, onSelectPlatform }: Props) => {
+  return (
+    <>
+      <HStack>
+        <Text>Orderby:</Text>
+        <Select
+          placeholder="Relevance"
+          onChange={(e) => {
+            onSelectOrderBy(e.target.value);
+          }}
+        >
+          <option value="added">Date added</option>
+          <option value="name">Name</option>
+          <option value="released">Release date</option>
+          <option value="metacritic">Popularity</option>
+          <option value="rating">Average rating</option>
+        </Select>
+      </HStack>
+      <Select
+        placeholder="All Platforms"
+        onChange={(e) => {
+          onSelectPlatform(e.target.value);
+        }}
+      >
+        <option value="pc">PC</option>
+        <option value="playstation">Playstation</option>
+        <option value="xbox">Xbox</option>
+        <option value="nintendo">Nintendo</option>
+        <option value="mac">Mac</option>
+        <option value="linux">Linux</option>
+        <option value="ios">IOS</option>
+        <option value="android">Android</option>
+      </Select>
+    </>
+  );
+};
+
+export default SearchBar;
