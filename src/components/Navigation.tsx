759566b14e45ecf19f01a1608645d5c08a18cc96
肖帆 <xiaofandev@126.com>
2025-03-22 18:37:56 +0800

    Feature: Remove the feature of color mode switch.

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,6 +1,5 @@
 import { Flex } from "@chakra-ui/react";
 import SearchInput from "./SearchInput";
-import ColorSwitch from "./ColorModeSwitch";
 import Logo from "./Logo";
 
 interface Props {
@@ -12,7 +11,6 @@ const Navigation = ({ onSearch }: Props) => {
     <Flex p={4} justifyContent={"space-between"}>
       <Logo />
       <SearchInput onSearch={onSearch} />
-      <ColorSwitch />
     </Flex>
   );
 };
