1c7a9cad0702c67d25c0145ed4412319b89a86f0
xiaofandev <xiaofandev@126.com>
2025-05-09 23:09:05 +0800

    Refactor: create useable apiClient.

--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,12 +1,17 @@
-import useData from "./useData";
+import { useQuery } from "@tanstack/react-query";
+import APIClient from "../api/apiClient";
 
 export interface Platform {
   id: string;
   name: string;
 }
 
+const apiClient = new APIClient<Platform>("/platforms");
 const usePlatforms = () => {
-  return useData<Platform>(["platforms"], "/platforms");
+  return useQuery<Platform[], Error>({
+    queryKey: ["platforms"],
+    queryFn: apiClient.getAll,
+  });
 };
 
 export default usePlatforms;
