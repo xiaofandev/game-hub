2024abb0fc0784442a2690f380e04002e80a40ee
xiaofandev <xiaofandev@126.com>
2025-05-22 14:57:13 +0800

    Refactor: remove interface from hook.

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,6 +1,6 @@
 import { useQuery } from "@tanstack/react-query";
 import APIClient from "../api/apiClient";
-import { Genre } from "./Genre";
+import { Genre } from "../entities/Genre";
 
 const apiClient = new APIClient<Genre>("/genres");
 
