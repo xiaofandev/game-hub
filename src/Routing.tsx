7b052fa51fae8a65ec35d26dc541733386f18fa3
xiaofandev <xiaofandev@126.com>
2025-05-22 09:30:06 +0800

    Feature: Set up routing for page not found

--- src/Routing.tsx
+++ src/Routing.tsx
@@ -2,13 +2,15 @@ import { Route, Routes } from "react-router";
 import Layout from "./pages/Layout";
 import HomePage from "./pages/HomePage";
 import GameDetails from "./pages/GameDetails";
+import PageNotFound from "./pages/PageNotFound";
 
 const Routing = () => {
   return (
     <Routes>
       <Route element={<Layout />}>
         <Route index element={<HomePage />} />
-        <Route path="/games/:id" element={<GameDetails />} />
+        <Route path="games/:id" element={<GameDetails />} />
+        <Route path="*" element={<PageNotFound />} />
       </Route>
     </Routes>
   );
---
+++ src/pages/PageNotFound.tsx
@@ -0,0 +1,11 @@
+import { Box, Text } from "@chakra-ui/react";
+
+const PageNotFound = () => {
+  return (
+    <Box p={5}>
+      <Text>Page not found.</Text>
+    </Box>
+  );
+};
+
+export default PageNotFound;
