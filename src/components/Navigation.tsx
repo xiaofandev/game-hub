73c2f85750f09a6205bbc330cfa69b27ffd216ef
xiaofandev <xiaofandev@126.com>
2025-05-23 16:54:39 +0800

    Fix the navigation

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,11 +1,14 @@
 import { HStack } from "@chakra-ui/react";
 import SearchInput from "./SearchInput";
 import Logo from "./Logo";
+import { Link } from "react-router";
 
 const Navigation = () => {
   return (
     <HStack p={4}>
-      <Logo />
+      <Link to="/">
+        <Logo />
+      </Link>
       <SearchInput />
     </HStack>
   );
--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -2,16 +2,20 @@ import { Input, InputGroup, InputLeftElement } from "@chakra-ui/react";
 import { useRef } from "react";
 import { BsSearch } from "react-icons/bs";
 import useQueryParamStore from "../store";
+import { useNavigate } from "react-router";
 
 const SearchInput = () => {
   const setSearchText = useQueryParamStore((state) => state.setSearchText);
 
   const ref = useRef<HTMLInputElement>(null);
+
+  const navigate = useNavigate();
   return (
     <form
       onSubmit={(e) => {
         e.preventDefault();
         setSearchText(ref.current?.value || "");
+        navigate("/");
       }}
     >
       <InputGroup>
